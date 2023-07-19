a5a72a7698ec07b7ac75c66b3143bfd33c97c18e
Imam Prasetyo <99059438+immprasetyo@users.noreply.github.com>
2023-07-19 20:55:09 +0700

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -10,7 +10,7 @@ const app = express();
 // Load environment variables from .env file
 //dotenv.config();
 
-const db_conn_string = process.env.DB_CONN_STRING;
+const db_conn_string = process.env.db_conn_string_env;
 
 mongoose.connect(db_conn_string, {
     useNewUrlParser: true,
